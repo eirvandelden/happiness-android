8c1af82f92c0e55f32e407080a7c90c283f50160
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-08-25 16:00:06 +0200

    feat: open happiness on Android

---
+++ app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "family.vandelden.happiness"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "family.vandelden.happiness"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.hotwire.core)
+    implementation(libs.hotwire.navigation.fragments)
+}
